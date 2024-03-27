685383c1a61267c27db1888139c17ee036b5891f
Felipe Mattioli dos Santos <felipe.mattiolidossantos@msc.com>
2024-03-27 11:02:31 +0100

    FEAT: Adding sasl support to kafka and changing sql to use postgres

--- src/CrossCutting/Extensions/UnitOfWork/UnitOfWorkExtensions.cs
+++ src/CrossCutting/Extensions/UnitOfWork/UnitOfWorkExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using Data.Persistence.UnitOfWork;
 using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
 using System.Data;
-using System.Data.SqlClient;
 
 namespace CrossCutting.Extensions.UnitOfWork
 {
@@ -10,10 +10,10 @@ namespace CrossCutting.Extensions.UnitOfWork
         public static IServiceCollection AddUnitOfWork(this IServiceCollection services, string connectionString)
         {
             services.AddScoped<IUnitOfWork, Data.Persistence.UnitOfWork.UnitOfWork>();
-            services.AddScoped((s) => new SqlConnection(connectionString));
+            services.AddScoped((s) => new NpgsqlConnection(connectionString));
             services.AddScoped<IDbTransaction>(s =>
             {
-                SqlConnection conn = s.GetRequiredService<SqlConnection>();
+                NpgsqlConnection conn = s.GetRequiredService<NpgsqlConnection>();
                 conn.Open();
                 return conn.BeginTransaction();
             });
--- src/Data/Persistence/Repositories/SpendManagementEventRepository.cs
+++ src/Data/Persistence/Repositories/SpendManagementEventRepository.cs
@@ -1,17 +1,17 @@
 ﻿using Dapper;
 using Domain.Entities;
 using Domain.Interfaces;
+using Npgsql;
 using Serilog;
 using System.Data;
-using System.Data.SqlClient;
 using System.Text;
 
 namespace Data.Persistence.Repositories
 {
-    public class SpendManagementEventRepository(SqlConnection sqlConnection, ILogger logger, IDbTransaction dbTransaction)
+    public class SpendManagementEventRepository(NpgsqlConnection sqlConnection, ILogger logger, IDbTransaction dbTransaction)
         : ISpendManagementEventRepository
     {
-        private readonly SqlConnection _sqlConnection = sqlConnection;
+        private readonly NpgsqlConnection _dbConnection = sqlConnection;
         private readonly IDbTransaction _dbTransaction = dbTransaction;
         private readonly ILogger _logger = logger;
         private readonly StringBuilder QueryBuilder = new();
@@ -19,14 +19,13 @@ namespace Data.Persistence.Repositories
         public async Task<Guid> Add(SpendManagementEvent spendManagementEvent)
         {
             QueryBuilder.Clear();
-            QueryBuilder.AppendLine("INSERT INTO [SpendManagementEvents]");
+            QueryBuilder.AppendLine("INSERT INTO SpendManagementEvents");
             QueryBuilder.AppendLine("( ");
-            QueryBuilder.AppendLine("[RoutingKey], ");
-            QueryBuilder.AppendLine("[DataEvent], ");
-            QueryBuilder.AppendLine("[NameEvent], ");
-            QueryBuilder.AppendLine("[EventBody] ");
+            QueryBuilder.AppendLine("RoutingKey, ");
+            QueryBuilder.AppendLine("DataEvent, ");
+            QueryBuilder.AppendLine("NameEvent, ");
+            QueryBuilder.AppendLine("EventBody ");
             QueryBuilder.AppendLine(") ");
-            QueryBuilder.AppendLine("OUTPUT Inserted.RoutingKey ");
             QueryBuilder.AppendLine(" VALUES ");
             QueryBuilder.AppendLine(" ( ");
             QueryBuilder.AppendLine("  @RoutingKey,  ");
@@ -35,7 +34,7 @@ namespace Data.Persistence.Repositories
             QueryBuilder.AppendLine("  @EventBody  ");
             QueryBuilder.AppendLine("  )  ");
 
-            var result = await _sqlConnection.ExecuteScalarAsync(QueryBuilder.ToString(), spendManagementEvent, _dbTransaction);
+            var result = await _dbConnection.ExecuteScalarAsync(QueryBuilder.ToString(), spendManagementEvent, _dbTransaction);
 
             if (result != null && Guid.TryParse(result.ToString(), out Guid guidResult))
             {
@@ -46,6 +45,6 @@ namespace Data.Persistence.Repositories
             _logger.Information("An error occured when tried insert the command", spendManagementEvent);
             return Guid.Empty;
         }
-       
+
     }
 }
